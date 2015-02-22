91e6df8e4e3611e568716a69673885d1180f1fd3
daicham <daicham@gmail.com>
2015-02-22 15:38:02 +0900

    Add a target VM and provisioning for ansible

--- Vagrantfile
+++ Vagrantfile
@@ -9,5 +9,11 @@ Vagrant.configure(2) do |config|
   config.vm.box = "centos7"
   config.vm.box_url = "https://f0fff3908f081cb6461b407be80daf97f07ac418.googledrive.com/host/0BwtuV7VyVTSkUG1PM3pCeDJ4dVE/centos7.box"
 
-  config.vm.provision :shell, :path => "provision.sh"
+  config.vm.define "control" do |control|
+    control.vm.network "private_network", ip: "192.168.33.100"
+    control.vm.provision :shell, :path => "provision.sh"
+  end
+  config.vm.define "dev" do |dev|
+    dev.vm.network "private_network", ip: "192.168.33.101"
+  end
 end
--- provision.sh
+++ provision.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-# ===============================
-# PROVISIONING SCRIPT For CentOS
-# ===============================
+# =============================================
+# Provisioning Script For Ansible on CentOS
+#   arg1: VM Name (config.vm.define="VM NAME")
+# =============================================
 
+#
 # Install EPEL
+#
 yum install epel-release -y
 
 # Disable EPEL Repository
-mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.org
+cp -p /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.org
 sed -e 's/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo.org > /etc/yum.repos.d/epel.repo
 
+#
 # Install Ansible
+#
 yum install ansible -y --enablerepo=epel
+
+# Copy SSH config for connecting target VM
+mkdir -p /vagrant/.ssh
+cp /vagrant/ssh_config /home/vagrant/.ssh/config
